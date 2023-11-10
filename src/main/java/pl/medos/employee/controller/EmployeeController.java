16f051b7158467430d69494c2fbbb6fc2b89ffd3
Krzysztof <krzysztof@woelke.pl>
2023-11-10 14:33:14 +0100

    add findByname service method

--- src/main/java/pl/medos/employee/controller/EmployeeController.java
+++ src/main/java/pl/medos/employee/controller/EmployeeController.java
@@ -3,13 +3,11 @@ package pl.medos.employee.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.medos.employee.model.Employee;
 import pl.medos.employee.service.EmployeeService;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -22,11 +20,27 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
-    @GetMapping
+    @GetMapping()
     public String getListOfEmployees(Model model) {
-
+        LOGGER.info("getAllOfEmployees");
         List<Employee> employees = employeeService.getEmployees();
         model.addAttribute("employees", employees);
         return "list-info";
+    }
+
+    @GetMapping("/search")
+    public String searchEmployeeByName(
+            @RequestParam(value = "employeeName") String query,
+//            @RequestParam(name = "pageNo", defaultValue = "1") int pageNo,
+            Model model){
+        LOGGER.info("searchUriByName()");
+//        int pagesize =10;
+
+        List<Employee> employeeByName = employeeService.getEmployeeByname(query);
+//        Page<Employee> employeeByName = employeeService.findEmployeeByName(pageNo, pagesize, query);
+        LOGGER.info("findEmployeeByName() + " + employeeByName);
+        model.addAttribute("employees", employeeByName);
+//        model.addAttribute("currentPage", pageNo);
+        return "list-info.html";
     }
 }
--- src/main/java/pl/medos/employee/service/EmployeeServiceImpl.java
+++ src/main/java/pl/medos/employee/service/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package pl.medos.employee.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import pl.medos.employee.controller.EmployeeController;
 import pl.medos.employee.model.Employee;
@@ -26,4 +27,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         return Arrays.asList(employees);
     }
 
+    @Override
+    public List<Employee> getEmployeeByname(@RequestParam(name = "name") String name) {
+        LOGGER.info("getEmployeeByNameApi");
+        String employeeUri = externalServerURL+"/employees/findByName/"+name;
+        RestTemplate restTemplate = new RestTemplate();
+        Employee[] employees = restTemplate.getForObject(employeeUri, Employee[].class);
+        return Arrays.asList(employees);
+    }
 }
